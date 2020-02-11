006160be34094ed3b47c40f0fe988c9247452a40
danjosepad <danielpadilhapessoal@outlook.com>
2020-02-11 20:17:44 -0300

    feat: base structure for GoBarber

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import auth from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
+routes.use(auth);
+
+routes.put('/users', UserController.update);
 export default routes;
